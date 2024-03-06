3ae75933c01b216aa749e67c1505ae7f45ed7ebc
evvasilieva <143832137+evvasilieva@users.noreply.github.com>
2024-03-06 16:07:20 +0300

    Initial commit

---
+++ gbhtproject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gbhtproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gbhtproject.settings")
+
+application = get_wsgi_application()
